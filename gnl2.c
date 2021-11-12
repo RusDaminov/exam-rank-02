01dd59df5c7c4322fed27acad3f293f951bf012b
Arshavin Bernita <abernita@ge-e5.kzn.21-school.ru>
2021-11-12 17:09:13 +0300

    +gnl2

---
+++ gnl2.c
@@ -0,0 +1,22 @@
+int  get_next_line (char **line)
+{
+    int i;
+    char buff;
+
+    i = 0;
+    if (line == NULL)
+        return (-1);
+    *line = malloc(sizeof(char) * 100);
+    while (read(0, &buff, 1) > 0)
+    {
+        if (buff == '\n')
+        {
+            (*line)[i] = '\0';
+            return (1);
+        }
+        (*line)[i] = buff;
+        i++;
+    }
+    (*line)[i] = '\0';
+    return (0);
+}
